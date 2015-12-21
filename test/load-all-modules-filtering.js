94e1d2ecb694e2ed425932d10904a338837109c8
Marcello de Sales <Marcello_DeSales@intuit.com>
2015-12-20 16:07:48 -0800

    Adding test cases for filtering

---
+++ test/load-all-modules-filtering.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var path = require("path");
+var expect = require("chai").expect;
+
+var dirquire = require("../lib");
+
+describe("dirquire", function() {
+
+  describe("Loading proper directory", function() {
+
+    it("should load all modules in a dir and return the array", function(done) {
+
+      // Use the fixture that's without problems
+      var dir = path.resolve("fixtures", "all-modules-correct");
+      var filter = { extension: "cure"};
+
+      dirquire(dir, filter).forEach(function modulesIterator(dirApi) {
+        expect(dirApi.filePath).to.not.be.null;
+        expect(dirApi.fileName).to.not.be.null;
+        expect(dirApi.module).to.not.be.null;
+      });
+
+      done();
+
+    });
+
+  });
+
+});
